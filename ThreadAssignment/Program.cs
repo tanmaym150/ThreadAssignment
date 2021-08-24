ba71be138897df95df319a5241ec62cc3c8c2fb4
tanmaym150 <tanmaymahamulkar150@gmail.com>
2021-08-24 14:04:44 +0530

    Add project files.

---
+++ ThreadAssignment/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadAssignment
+{   //What are the methods used in Thread Class?
+    //1.Abort() : This method is used to destroy threads by throwing ThreadAbortException
+    //2.Interrupt() :This method interrupts the thread from a blocked state.
+    //3.Join() :This method allows one thread to waituntil another thread completes its execution.
+    //4.ResetAbort() :This method is responsible for cancelling the abort request of the current thread.
+    //5.Resume() :Method is used to resume the suspended thread
+    //6.Sleep() :This method is used to temporarily suspend the current execution of the thread for specefied milliseconds.
+    //7.Start() :This method is used to send a thread into runnable state
+    //8.Suspend() :Used to suspend the thread
+    //9.Yield() :Another thread is processed.
+    
+
+    class Program
+    {   static void TableOf2()
+        {
+            Console.WriteLine("Table of 2:");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i*2);
+            }
+        }
+        static void TableOf3()
+        {
+            Console.WriteLine("Table of 3:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * 3);
+            }
+
+        }
+        static void TableOf4()
+        {
+            Console.WriteLine("Table of 4:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i * 4);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(TableOf2);
+            Thread t2 = new Thread(TableOf3);
+            Thread t3 = new Thread(TableOf4);
+            //OUTPUT 1
+            //t1.Start();
+            //t1.Join();
+            //t2.Start();
+            //t2.Join();
+            //t3.Start();
+            //t3.Join();
+
+
+
+
+            
+            //OUTPUT 2
+            t3.Start();
+            t3.Join();
+            t2.Start();
+            t2.Join();
+            t1.Start();
+            t1.Join();
+
+            ThreadProperties.CurrentThead();
+            ThreadProperties.ManagedThreadId();
+            ThreadProperties.ThreadPriorityCheck();
+            ThreadProperties.ThreadSleep();
+
+           
+
+
+        }
+    }
+}
---
+++ ThreadAssignment/ThreadProperties.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadAssignment
+{
+    public class ThreadProperties
+    {  
+       public static void CurrentThead()
+        {   //Gives the name of current thread.
+            Thread thr;
+            thr = Thread.CurrentThread;
+            thr.Name = "IX Thread";
+            Console.WriteLine("The name of the current thread is :{0}",thr.Name);
+            Console.WriteLine("Is the {0} alive? :{1}",thr.Name,thr.IsAlive);
+           
+
+
+        }
+        public static void ManagedThreadId()
+        {
+            Thread t4 = new Thread(new ThreadStart(ManagedThreadId));
+            Console.WriteLine("The managed thread id of thread IX is:{0}",t4.ManagedThreadId);
+        } 
+        public static void ThreadPriorityCheck()
+        {
+            Thread t11 = new Thread(ThreadPriorityCheck);
+            Thread t12 = new Thread(ThreadPriorityCheck);
+            Thread t13 = new Thread(ThreadPriorityCheck);
+
+            //setting the priority of thread
+            t12.Priority = ThreadPriority.Highest;
+            t13.Priority = ThreadPriority.BelowNormal;
+
+            Console.WriteLine("The priority of t11 is:{0}",t11.Priority);//normal-2
+            Console.WriteLine("The priority of t12 is:{0}", t12.Priority);//highest-4
+            Console.WriteLine("The priority of t13 is:{0}", t13.Priority);//belownormal-0
+
+            //ThreadState
+            Console.WriteLine("the threadstate of t12 is: {0}  ",t12.ThreadState);//unstarted
+
+
+
+
+        }
+        public static void ThreadSleep()
+        {
+            Thread ts = new Thread(ThreadSleep);
+           
+            for(int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    Thread.Sleep(2000);
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
